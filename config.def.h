f433bf83ec825abbfac35863bfd50424a844aae5
joanjajas <joanbeltranperis@gmail.com>
2022-06-11 21:38:17 +0200

    added colorbar patch

--- config.def.h
+++ config.def.h
@@ -7,17 +7,24 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+
+/* fonts */
 static const char *fonts[]          = { "Caskaydia Cove Nerd Font:size=10" };
 static const char dmenufont[]       = "Caskaydia Cove Nerd Font:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+
+/* colors */
+static const char col_bg[] = "#0f161e";
+static const char col_fg[] = "#76776f";
+
+static const char *colors[][3] = {
+	/*                      fg     bg      border   */
+	[SchemeNorm]     = { col_fg, col_bg, "#000000" },
+	[SchemeSel]      = { col_fg, col_bg, "#000000" },
+	[SchemeStatus]   = { col_fg, col_bg, "#000000" }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_fg, col_bg, "#000000" }, // Tagbar left selected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm] = { col_fg, col_bg, "#000000" }, // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]  = { col_fg, col_bg, "#000000" }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm] = { col_fg, col_bg, "#000000" }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
@@ -60,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_bg, "-sf", col_fg, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
